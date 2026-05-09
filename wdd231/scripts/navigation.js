f086a52f94d97d6aee191690d2e0ff7ead666626
AeuroShaw <feyuro@gmail.com>
2026-05-09 12:35:40 +0100

    Delete wdd231 directory

--- wdd231/scripts/navigation.js
+++
@@ -1,6 +0,0 @@
-const menuButton = document.querySelector('#menu');
-const navigation = document.querySelector('nav');
-
-menuButton.addEventListener('click', () => {
-    navigation.classList.toggle('open');
-});
